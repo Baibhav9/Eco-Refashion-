69bc71cd932ad7af1e8191cbde2691025868500f
Baibhav9 <baibhavmac@gmail.com>
2025-04-28 00:46:30 +0545

    Fix Overall Page UI and Functionality

--- apps/main/forms.py
+++ apps/main/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from .models import SellerProduct, UserProduct, Order
 
+from .constants import Roles, ProductCategory
+from .models import Product, Order
 
-class SellerProductForm(forms.ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
-        model = SellerProduct
+        model = Product
         fields = [
             'title', 
             'description', 
@@ -12,23 +13,35 @@ class SellerProductForm(forms.ModelForm):
             'category',
             'available_size',
             'color',
-            'price'
+            'price',
+            'stock_amount',
+            'delivery_option',
+            'pickup_address',
+            'pickup_phone'
         ]
         labels = {
             "image": "Image",
         }
 
-class UserProductForm(forms.ModelForm):
-    class Meta:
-        model = UserProduct
-        fields = ['title', 'description', 'image', 'price']
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        category = kwargs.pop('category', None)
+        super().__init__(*args, **kwargs)
 
-    def clean(self):
-        cleaned_data = super().clean()
-        # Ensure rental duration is set only if item is marked as rentable
-        if cleaned_data.get('is_rentable') and cleaned_data.get('rental_duration') > 7:
-            raise forms.ValidationError("Rental duration cannot exceed 7 days.")
-        return cleaned_data
+        self.fields["category"].choices = [
+            (category, category.title()),
+        ]
+
+        if category == ProductCategory.DONATION or category == ProductCategory.RECYCLE:
+            if "price" in self.fields:
+                del self.fields["price"]
+
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ == 'Select':
+                field.widget.attrs['class'] = 'form-select my-3'
+            else:
+                field.widget.attrs['class'] = 'form-control my-3'
+            field.widget.attrs['placeholder'] = f'Enter {field.label}'
 
 class OrderForm(forms.ModelForm):
     class Meta:
